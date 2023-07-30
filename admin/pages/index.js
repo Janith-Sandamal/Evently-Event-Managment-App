10a2a09cd57f5ac7b6c925048c8b542bc91e1068
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2023-07-30 06:33:56 +0530

    logged in button setup complete with google auth✅

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,10 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  return (
-   <div className={'bg-blue-900 w-screen h-screen flex items-center'}>
+  const { data: session } = useSession();
+  if(!session){
+    return (
+      <div className={'bg-blue-900 w-screen h-screen flex items-center'}>
       <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
       </div>
     </div>
+    );
+  }
+  return (
+   <div>logged in {session.user.email}</div>
   )
 }
